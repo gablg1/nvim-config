90fb0f577bed589500ced05173a76e04877eb28c
Gabriel Guimaraes <gabriellimaguimaraes@gmail.com>
2024-10-15 15:53:17 -0300

    basic

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -3,8 +3,8 @@ vim.cmd("let g:netrw_liststyle = 3")
 local opt = vim.opt -- for conciseness
 
 -- line numbers
-opt.relativenumber = true -- show relative line numbers
-opt.number = true -- shows absolute line number on cursor line (when relative number is on)
+-- opt.relativenumber = false -- show relative line numbers
+-- opt.number = true -- shows absolute line number on cursor line (when relative number is on)
 
 -- tabs & indentation
 opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
